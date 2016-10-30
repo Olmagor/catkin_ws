647c095dc19b2733f534a65d9841dd539df20727
Olmagor <dillier_007@hotmail.com>
2016-10-30 10:56:40 +0100

    Update rosCustomMultiCtr.bash

--- rosCustomMultiCtr.bash
+++ rosCustomMultiCtr.bash
@@ -22,31 +22,31 @@ then
     tmux send-keys "sudo -i" C-m
     tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
     tmux send-keys "rosrun navio2_remote remote_multiCtr $1 $2 $3 $4 $5" C-m
-elif ([ "$#" -eq 6] && ["$6" == "-log"])
-then
-    echo "Here we go WITH log files"
-    sleep 2
-    tmux -2 new-session -d -s $SESSION
-    tmux new-window -t $SESSION:1 -n 'ROS'
-    tmux split-window -h
-    tmux select-pane -t 0
-    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "roscore" C-m
-    tmux select-pane -t 1
-    tmux send-keys "sleep 5" C-m
-    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "rosrun navio2_imu imu_pub $(($1-1))" C-m
-    tmux select-pane -t 0
-    tmux split-window -v
-    tmux send-keys "sleep 5" C-m
-    tmux send-keys "sudo -i" C-m
-    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "rosrun navio2_remote remote_multiCtr $1 $2 $3 $4 $5" C-m
-    tmux split-window -v
-    tmux send-keys "sleep 10" C-m
-    tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "cd /home/pi/bagfiles" C-m
-    tmux send-keys "rosbag record -a" C-m
+    elif ([ "$#" -eq 6] && ["$6" == "-log"])            #to active the writing of the log files
+    then
+        echo "Here we go WITH log files"
+        sleep 2
+        tmux -2 new-session -d -s $SESSION
+        tmux new-window -t $SESSION:1 -n 'ROS'
+        tmux split-window -h
+        tmux select-pane -t 0
+        tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+        tmux send-keys "roscore" C-m
+        tmux select-pane -t 1
+        tmux send-keys "sleep 5" C-m
+        tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+        tmux send-keys "rosrun navio2_imu imu_pub $(($1-1))" C-m
+        tmux select-pane -t 0
+        tmux split-window -v
+        tmux send-keys "sleep 5" C-m
+        tmux send-keys "sudo -i" C-m
+        tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+        tmux send-keys "rosrun navio2_remote remote_multiCtr $1 $2 $3 $4 $5" C-m
+        tmux split-window -v
+        tmux send-keys "sleep 10" C-m                                       #no needs to start earlier because 10/15 sec of calibration
+        tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
+        tmux send-keys "cd /home/pi/bagfiles" C-m
+        tmux send-keys "rosbag record -a" C-m
 else
     echo "Usage : ./rosCustom.bash [freq] [saturation] [Kp] [Ki] [Kd] [-log], -log is not necessary"
     exit 0
