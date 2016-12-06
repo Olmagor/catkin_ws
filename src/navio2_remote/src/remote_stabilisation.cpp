66db05ec764ae9a32b8bd4959a6cd6dac97bf96d
Olmagor <dillier_007@hotmail.com>
2016-12-06 15:24:25 +0100

    Update remote_stabilisation.cpp

--- src/navio2_remote/src/remote_stabilisation.cpp
+++ src/navio2_remote/src/remote_stabilisation.cpp
@@ -30,7 +30,7 @@ void read_Imu(sensor_msgs::Imu imu_msg)
 
 	//current roll angle, positif value in clockwise
 	currentRoll = imu_msg.orientation.x;		
-	currentRollSpeed = imu_msg.angular_velocity.x;
+	currentRollSpeed = 0; //imu_msg.angular_velocity.x;
 }
 
 int main(int argc, char **argv)
