ff6f9f93e512aec711fd1cb5cde537ee26059d33
Olmagor <dillier_007@hotmail.com>
2016-12-08 09:47:16 +0100

    Update remote_stabilisation.cpp

--- src/navio2_remote/src/remote_stabilisation.cpp
+++ src/navio2_remote/src/remote_stabilisation.cpp
@@ -12,7 +12,7 @@
 #define PILOT_TRIM 1410.0f
 #define PI 3.14159
 
-float K1 = -3.9351;	//-64.1997/12; //-64.1997;
+float K1 = -3.7074;	//-64.1997/12; //-64.1997;
 float K2 = -0.4094;	//-1.908;
 float u = 0;
 float amplitude = K1;
