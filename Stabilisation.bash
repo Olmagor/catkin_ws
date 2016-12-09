b54004f6944ce1465acd5c18e5fa453864c333a3
Olmagor <dillier_007@hotmail.com>
2016-12-09 14:12:49 +0100

    Update Stabilisation.bash

--- Stabilisation.bash
+++ Stabilisation.bash
@@ -2,7 +2,7 @@
 SESSION=$USER
 
 #no log
-if [ "$#" -eq 2 ]
+if [ "$#" -eq 3 ]
 then
     echo "Here we go in STABILISATION MODE"
     sleep 2
@@ -21,8 +21,8 @@ then
     tmux send-keys "sleep 5" C-m
     tmux send-keys "sudo -i" C-m
     tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "rosrun navio2_remote remote_stabilisation $1 $2" C-m
-elif [ "$#" -eq 3 ] && [ "$3" == '-log' ]          #to active the writing of the log files
+    tmux send-keys "rosrun navio2_remote remote_stabilisation $1 $2 $3" C-m
+elif [ "$#" -eq 4 ] && [ "$4" == '-log' ]          #to active the writing of the log files
   then
       echo "Here we go in STABILISATION MODE, WITH log files"
       sleep 2
@@ -41,14 +41,15 @@ elif [ "$#" -eq 3 ] && [ "$3" == '-log' ]          #to active the writing of the
       tmux send-keys "sleep 5" C-m
       tmux send-keys "sudo -i" C-m
       tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-      tmux send-keys "rosrun navio2_remote remote_stabilisation $1" C-m
+      tmux send-keys "rosrun navio2_remote remote_stabilisation $1 $2 $3" C-m
       tmux split-window -v
       tmux send-keys "sleep 5" C-m                                       #no needs to start earlier because no calibration
       tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
       tmux send-keys "cd /home/pi/bagfiles" C-m
       tmux send-keys "rosbag record -a" C-m
 else
-    echo "Usage :   ./stabilisation.bash [K1] [k2] [-log], -log is not necessary" 
+    echo "Usage :   ./stabilisation.bash [K1] [K2] [K3] [-log], -log is not necessary"
+    echo "In case pid: Kp=K1, Ki=K2, Kd=K3"
     exit 0
 fi
 
