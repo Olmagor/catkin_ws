0ccbec9d3a5987226cf6c3f4f321a4cd369bab0c
Olmagor <dillier_007@hotmail.com>
2016-12-13 15:57:24 +0100

    Update prbsPascal.bash

--- prbsPascal.bash
+++ prbsPascal.bash
@@ -2,7 +2,7 @@
 SESSION=$USER
 
 #no log
-if [ "$#" -eq 4 ]
+if [ "$#" -eq 5 ]
 then
     echo "Here we go in IDENTIFICATION PRBS MODE"
     sleep 2
@@ -21,8 +21,8 @@ then
     tmux send-keys "sleep 5" C-m
     tmux send-keys "sudo -i" C-m
     tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-    tmux send-keys "rosrun navio2_remote remote_prbs_pascal $1 $2 $3 $4" C-m
-elif [ "$#" -eq 5 ] && [ "$5" == '-log' ]          #to active the writing of the log files
+    tmux send-keys "rosrun navio2_remote remote_prbs_pascal $1 $2 $3 $4 $5" C-m
+elif [ "$#" -eq 6 ] && [ "$6" == '-log' ]          #to active the writing of the log files
   then
       echo "Here we go in IDENTIFICATION PRBS MODE, WITH log files"
       sleep 2
@@ -41,16 +41,17 @@ elif [ "$#" -eq 5 ] && [ "$5" == '-log' ]          #to active the writing of the
       tmux send-keys "sleep 5" C-m
       tmux send-keys "sudo -i" C-m
       tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
-      tmux send-keys "rosrun navio2_remote remote_prbs_pascal $1 $2 $3" C-m
+      tmux send-keys "rosrun navio2_remote remote_prbs_pascal $1 $2 $3 $4 $5" C-m
       tmux split-window -v
       tmux send-keys "sleep 10" C-m                                       #no needs to start earlier because 10/15 sec of calibration
       tmux send-keys "source /home/pi/catkin_ws/devel/setup.bash" C-m
       tmux send-keys "cd /home/pi/bagfiles" C-m
       tmux send-keys "rosbag record -a" C-m
 else
-    echo "Usage :   ./rosCustom.bash [prbs amp] [freq prbs] [freq ros] [MaxThrottlePwm] [-log], -log is not necessary" #MaxThrottlePwm is limited to 2000 in the cpp
+    echo "Usage :   ./rosCustom.bash [prbs amp] [freq prbs] [freq ros] [MaxThrottlePwm] [actuator] [-log], -log is not necessary" #MaxThrottlePwm is limited to 2000 in the cpp
     echo "[prbs amp] can be a value between 0 and 450 but best between 0 and 100, [prbs freq] must be a positive value usually between 20-100hz"
     echo "[freq ros] must be a positive value usually 50Hz, [MaxThrottlePwm] can be a value between 1500-2000ms"
+    echo "[actuator] is either 0 for pilot=prbs signal or 1 steering=prbs signal"
     exit 0
 fi
 
